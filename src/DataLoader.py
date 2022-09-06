7de831cc8354d3d2bce10475e84d6c87ab2011b8
Simon Butt <simonbutt123@gmail.com>
2022-09-07 00:16:14 +0100

    feat(fiftyone): Config driven metadata labels sourced from BQ query
    Signed-off-by: Simon Butt <simonbutt123@gmail.com>

--- src/DataLoader.py
+++ src/DataLoader.py
@@ -1,35 +1,77 @@
+from __future__ import annotations
 import fiftyone as fo
 import logging
-import os
 from typing import Type
+import pandas as pd
 import pathlib
+import time
 
 
 class DataLoader:
-    def __init__(self, volume_path: str) -> None:
+    def __init__(self, volume_path: str, project_id: str="") -> None:
         self.volume_path = volume_path
+        self.project_id = project_id
 
-    def _demo_dataset(self, demo_name: str="quickstart") -> Type[fo.Dataset]:
+    def _demo_dataset(self, demo_name: str = "quickstart") -> Type[fo.Dataset]:
         logging.info(f"Loading demo dataset: {demo_name}")
         import fiftyone.zoo as foz
+
         return foz.load_zoo_dataset(demo_name)
 
-    def load_data(self, config: dict) -> Type[fo.Dataset]:
-        
-        # patch fix: fix known bug in gcsfuse where you have to define a folder path before it is searchable in some cases.
+    def _load_bq(self, query: str, key: str) -> dict:
+        df_bq = pd.read_gbq(
+            query,
+            project_id=self.project_id
+        )
+        return df_bq.set_index(key).T.to_dict()
+
+    def load_dataset(self, config: dict, local: bool = True) -> Type[fo.Dataset]:
+        ts = int(time.time())
+
         data_path = f"{self.volume_path}/{config['bucket_path_prefix']}"
+        dataset_name = (
+            f"{config['name']}{'-{}'.format(ts) if local else '' }"
+        )
+        logging.info(f"Data path: {data_path}, Dataset name: {dataset_name}")
+
+        # patch fix: fix known bug in gcsfuse where you have to define a folder path before it is searchable in some cases.
         pathlib.Path(data_path).mkdir(parents=True, exist_ok=True)
-        logging.info(f"Data path: {data_path}")
 
-
-
-        # Load Data
-        dataset_raw = fo.Dataset.from_dir(
-            dataset_dir=data_path,
-            dataset_type=fo.types.ImageDirectory,
-            name=config["name"]
+        label_lookup = self._load_bq(
+            query=config["metadata_config"]["ground_truth"]["query"],
+            key=config["metadata_config"]["ground_truth"]["key"],
         )
 
+        # TODO: Utilise _value to remove hard coding
+        gt_annotations = {
+                _key : {
+                    f"{self.volume_path}/{k}" : "clean" if v["is_dirty"] == 0 else "dirty" 
+                    for k, v in label_lookup.items()
+                } for _key, _value in config["metadata_config"].items()
+            }
+        
+        logging.debug(f"GT Annotations: {gt_annotations}")
 
+        # Create samples for your data
+        samples = []
+        for filepath in [
+            str(fp.absolute()) for fp in list(pathlib.Path(data_path).glob("**/*"))
+        ]:
+            sample = fo.Sample(filepath=filepath)
 
-        return dataset_raw
+            try:
+                for k in config["metadata_config"].keys():
+                    # Store classification in a field name of your choice
+                    label = gt_annotations[k][filepath]
+                    logging.debug(f"Label: {label}")
+                    sample[k] = fo.Classification(label=label)
+
+                samples.append(sample)
+            except KeyError as e:
+                logging.debug(f"Keyerror with filepath: {filepath}")
+
+        # Create dataset
+        dataset = fo.Dataset(name=dataset_name, persistent=config["persistent"])
+        dataset.add_samples(samples)
+
+        return dataset
--- src/main.py
+++ src/main.py
@@ -10,18 +10,17 @@ def _set_log_level(log_env: str) -> None:
     # Using approved_log_levels to reduce risk of using an eval
     approved_log_levels = {"DEBUG", "INFO", "WARNING", "ERROR"}
     log_level = (
-        "INFO"
-        if (env_result := log_env) not in approved_log_levels
-        else env_result
+        "INFO" if (env_result := log_env) not in approved_log_levels else env_result
     )
     logging.basicConfig(level=eval(f"logging.{log_level}"))
 
 
-def _get_dataset_config(path: str=".") -> dict:
+def _get_dataset_config(path: str = ".") -> dict:
     with open(f"{path}/dataset-config.yml") as dataset_config_file:
         dataset_config = yaml.load(dataset_config_file, Loader=yaml.SafeLoader)
     logging.debug(f"Dataset config: {dataset_config}")
     return dataset_config
+
 
 if __name__ == "__main__":
 
@@ -29,32 +28,31 @@ if __name__ == "__main__":
     data_path: str = os.environ.get("MNT_DIR")
     bucket_path_prefix: str = os.environ.get("BUCKET_PATH_PREFIX")
     is_demo: str = str(os.environ.get("IS_DEMO", "False"))
-    log_level: bool = os.environ.get("LOG_LEVEL")
+    log_level: str = os.environ.get("LOG_LEVEL")
+    project_id: str = os.environ.get("PROJECT_ID")
 
     _set_log_level(log_level)
 
     # Log data directory size
     data_dir_size: int = len(os.listdir(f"{data_path}/{bucket_path_prefix}"))
     logging.info(f"Objects in data directory: {data_dir_size}")
-    
 
     # Initiate DataLoader and create a FiftyOne set
     logging.info("Starting dataset load")
     dataset_config: dict = _get_dataset_config()
 
-    DL: Type[DataLoader] = DataLoader(volume_path=data_path)
+    DL: Type[DataLoader] = DataLoader(
+        volume_path=data_path,
+        project_id=project_id
+    )
     dataset: fo.Dataset = (
-        DL._demo_dataset() 
-        if is_demo.lower() == "true" else 
-        DL.load_data(config=dataset_config)
+        DL._demo_dataset()
+        if is_demo.lower() == "true"
+        else DL.load_dataset(config=dataset_config)
     )
 
     logging.info("Dataset created, starting fiftyone instance")
     # Start and maintain FiftyOne session
     # Port is hardcoded due to bug in fiftyone where changing the port breaks something internal
-    session: fo.Session = fo.launch_app(
-        dataset,
-        remote=True,
-        port=5151
-    )
+    session: fo.Session = fo.launch_app(dataset, remote=True, port=5151)
     session.wait()
